45e501886fe5f3b2d07db5ed46c756d6e4856c23
Francisval Cavalcante <francisvalcs@gmail.com>
2020-07-27 20:36:30 -0300

    Aplicação Filmes ReactJs - Projeto Filmes pt3

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -37,7 +37,7 @@ class Home extends Component{
                 <article key={filme.id}>
                   <strong>{filme.nome}</strong>
                   <img src={filme.foto} alt="capa"/>
-                  <Link to="/">Acessar</Link>
+                  <Link to={`/filme/${filme.id}`}>Acessar</Link>
                 </article>
               )
             })}
