2c09149236a0c9413b496cf6d6a573f01bfeea5a
Francisval Cavalcante <francisvalcs@gmail.com>
2020-07-26 22:07:15 -0300

    Aplicação Filmes ReactJs - Criação da página home e rotas de navegação

---
+++ src/routes.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import Home from './pages/Home';
+import Header from './components/Header';
+import Erro from './pages/Erro';
+
+const Routes = () => {
+  return(
+    <BrowserRouter>
+        <Header/>
+        <Switch>
+            <Route exact path="/" component={Home}/>
+            <Route path="*" component={Erro}/>
+        </Switch>
+    </BrowserRouter>
+  );
+}
+
+export default Routes;
